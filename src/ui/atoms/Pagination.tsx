4acb53390afff6dd4681c9a1881a9703af77634f
Zuzanna Gasior <zuzanna.gasior9@gmail.com>
2023-09-20 21:13:00 +0200

    fix: add additional pagination aria label

--- src/ui/atoms/Pagination.tsx
+++ src/ui/atoms/Pagination.tsx
@@ -10,7 +10,7 @@ const sharedClassName = "text-md p-2 font-semibold";
 
 export const Pagination = <T extends string>({ totalPages, hrefBuilder }: PaginationProps<T>) => {
 	return (
-		<nav className="mt-8" aria-label="Navigation">
+		<nav className="mt-8" aria-label="Navigation Pagination">
 			<ul className="flex justify-center space-x-2 text-neutral-500">
 				{Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
 					<li key={page}>
